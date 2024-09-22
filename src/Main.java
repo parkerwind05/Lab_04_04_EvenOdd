1933bb39095baeb253f8abd86ce26b69809fa5fe
parkerwind05 <parkerwind05@gmail.com>
2024-09-22 14:36:20 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int numToExamine = 8;
+        System.out.println("Your number is " + numToExamine);
+        if(numToExamine % 2 ==0) {
+            System.out.println("Your number is even");
+        }
+        else {
+            System.out.println("Your number is odd.");
+        }
+
+    }
+}
